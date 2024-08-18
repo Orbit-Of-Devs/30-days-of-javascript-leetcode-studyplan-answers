47bc6521b01b418c8f362ea9ac5e48048f483fc7
rezanahi <raad.thunder1@gmail.com>
2024-08-18 23:10:13 +0330

    add question 3

---
+++ 3-to-be-or-not-to-be.js
@@ -0,0 +1,20 @@
+var expect = function(val) {
+    
+    function toBe(x){
+        if(x === val){
+            return true
+        } else {
+            throw new Error ('Not Equal')
+        }
+    }
+
+    function notToBe(x){
+        if(x !== val){
+            return true
+        } else {
+            throw new Error ('Equal')
+        }
+    }
+
+    return {toBe, notToBe}
+};
